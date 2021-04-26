dc945616e4885dd59c71b305c98dcbabde105180
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-04-26 20:13:08 -0300

    projeto criado

---
+++ src/main/java/com/santos/LeilaoApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.santos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeilaoApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeilaoApiRestApplication.class, args);
+	}
+
+}
